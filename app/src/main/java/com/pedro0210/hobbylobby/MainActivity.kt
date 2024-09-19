6af8cac806e8f71ceb7aeb039aff04501d846405
PeDro0210 <pedroruben0210@gmail.com>
2024-09-19 16:18:42 -0600

    initial commit

---
+++ app/src/main/java/com/pedro0210/hobbylobby/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.pedro0210.hobbylobby
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pedro0210.hobbylobby.ui.theme.HobbyLobbyTheme
+
+
+//TODO: Set navigation graph
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HobbyLobbyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HobbyLobbyTheme {
+        Greeting("Android")
+    }
+}
